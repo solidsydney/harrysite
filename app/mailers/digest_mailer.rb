367f3ce49a7b9a903db204c372361f926567fa79
Uchenna Okafor <ramzauchenna@gmail.com>
2014-07-16 09:12:52 +0100

    added

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -2,6 +2,6 @@ class DigestMailer < ActionMailer::Base
   def send_daily_digest(subscriber)
     @message = message
     @email = subscriber.email
-    mail to: subscriber.email.gsub(",", ""), subject: "BlanckDigital Daily Digest", from: "no_reply@blanckdigital.com"
+    mail to: subscriber.email.gsub(",", ""), subject: "BlanckDigital Daily Digest", from: "\"BlanckDigital\" <noreply@blanckdigital.com>"
   end
 end

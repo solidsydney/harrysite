0ddf34c74628c537a20a3e053ed6e91d7a45e6a1
Uchenna Okafor <ramzauchenna@gmail.com>
2015-06-08 08:37:56 +0100

    fixed posts

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   def show
     if params[:id] && @category = Category.find(params[:id])
       @category = Category.includes(:posts).find(params[:id])
-      @posts = @category.posts.published.recent.page(params[:page]).per_page(27)
+      @posts = @category.posts.recent.page(params[:page]).per_page(27)
     else
       redirect_to root_url, notice: "Page Not Found"
     end

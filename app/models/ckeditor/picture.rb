83b734ea9cb66dcd077cdc47fd8a1cc4a9b1903d
uchenna <ramzauchenna@gmail.com>
2015-08-24 08:49:18 +0100

    do not validate attachments

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -5,6 +5,7 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :styles => { :content => '800>', :thumb => '118x100#' }
 
   validates_attachment_size :data, :less_than => 2.megabytes
+  do_not_validate_attachment_file_type :data
   validates_attachment_presence :data
   belongs_to :assetable, :polymorphic => true
   scope :recent, -> {order("RAND()")}
